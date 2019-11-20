2f22b30e574a193480919f173cd678246bf2c98c
Amber Ogata <amberogata@ambers-mbp.devices.brown.edu>
2019-11-19 23:28:02 -0500

    added rcnn file

---
+++ rcnn.py
@@ -0,0 +1,182 @@
+from __future__ import division
+import random
+import pprint
+import sys
+import time
+import numpy as np
+from optparse import OptionParser
+import pickle
+
+from keras import backend as K
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers import Input
+from keras.models import Model
+from keras_frcnn import config, data_generators
+from keras_frcnn import losses as losses
+import keras_frcnn.roi_helpers as roi_helpers
+from keras.utils import generic_utils
+from keras_frcnn.simple_parser import get_data
+
+C = config.Config()
+C.base_net_weights = nn.get_weight_path()
+train_imgs = []
+train = get_data('/content/stage_2_train_labels.csv')
+for _,row in train.iterrows():
+    img = []
+    img.append(row.patientId + '.dcm')
+    img.append(row.x)
+    img.append(row.y)
+    img.append(row.x + row.width)
+    img.append(row.y + row.height)
+    train_imgs.append(img)
+
+random.shuffle(all_imgs)
+
+num_imgs = len(all_imgs)
+
+# TODO: wtf is this
+data_gen_train = data_generators.get_anchor_gt(train_imgs, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='train')
+
+input_shape_img = (3, None, None)
+
+img_input = Input(shape=input_shape_img)
+roi_input = Input(shape=(None, 4))
+
+# define the base network (resnet here, can be VGG, Inception, etc)
+shared_layers = nn.nn_base(img_input, trainable=True)
+
+# define the RPN, built on the base layers
+num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
+rpn = nn.rpn(shared_layers, num_anchors)
+
+classifier = nn.classifier(shared_layers, roi_input, C.num_rois, nb_classes=len(classes_count), trainable=True)
+
+model_rpn = Model(img_input, rpn[:2])
+model_classifier = Model([img_input, roi_input], classifier)
+
+# this is a model that holds both the RPN and the classifier, used to load/save weights for the models
+model_all = Model([img_input, roi_input], rpn[:2] + classifier)
+
+try:
+	model_rpn.load_weights(C.base_net_weights, by_name=True)
+	model_classifier.load_weights(C.base_net_weights, by_name=True)
+except:
+	print('Could not load pretrained model weights. Weights can be found in the keras application folder \
+		https://github.com/fchollet/keras/tree/master/keras/applications')
+
+optimizer = Adam(lr=1e-5)
+optimizer_classifier = Adam(lr=1e-5)
+model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
+model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+model_all.compile(optimizer='sgd', loss='mae')
+
+epoch_length = 1000
+num_epochs = int(options.num_epochs)
+iter_num = 0
+
+losses = np.zeros((epoch_length, 5))
+rpn_accuracy_rpn_monitor = []
+rpn_accuracy_for_epoch = []
+start_time = time.time()
+
+best_loss = np.Inf
+
+class_mapping_inv = {v: k for k, v in class_mapping.items()}
+
+vis = True
+
+for epoch_num in range(num_epochs):
+
+	progbar = generic_utils.Progbar(epoch_length)
+	print('Epoch {}/{}'.format(epoch_num + 1, num_epochs))
+
+	while True:
+		try:
+			X, Y, img_data = next(data_gen_train)
+
+			loss_rpn = model_rpn.train_on_batch(X, Y)
+
+			P_rpn = model_rpn.predict_on_batch(X)
+
+			R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(), use_regr=True, overlap_thresh=0.7, max_boxes=300)
+			# note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
+			X2, Y1, Y2, IouS = roi_helpers.calc_iou(R, img_data, C, class_mapping)
+
+			if X2 is None:
+				rpn_accuracy_rpn_monitor.append(0)
+				rpn_accuracy_for_epoch.append(0)
+				continue
+
+			neg_samples = np.where(Y1[0, :, -1] == 1)
+			pos_samples = np.where(Y1[0, :, -1] == 0)
+
+			if len(neg_samples) > 0:
+				neg_samples = neg_samples[0]
+			else:
+				neg_samples = []
+
+			if len(pos_samples) > 0:
+				pos_samples = pos_samples[0]
+			else:
+				pos_samples = []
+
+			rpn_accuracy_rpn_monitor.append(len(pos_samples))
+			rpn_accuracy_for_epoch.append((len(pos_samples)))
+
+			if C.num_rois > 1:
+				if len(pos_samples) < C.num_rois//2:
+					selected_pos_samples = pos_samples.tolist()
+				else:
+					selected_pos_samples = np.random.choice(pos_samples, C.num_rois//2, replace=False).tolist()
+				try:
+					selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=False).tolist()
+				except:
+					selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=True).tolist()
+
+				sel_samples = selected_pos_samples + selected_neg_samples
+			else:
+				# in the extreme case where num_rois = 1, we pick a random pos or neg sample
+				selected_pos_samples = pos_samples.tolist()
+				selected_neg_samples = neg_samples.tolist()
+				if np.random.randint(0, 2):
+					sel_samples = random.choice(neg_samples)
+				else:
+					sel_samples = random.choice(pos_samples)
+
+			loss_class = model_classifier.train_on_batch([X, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
+
+			losses[iter_num, 0] = loss_rpn[1]
+			losses[iter_num, 1] = loss_rpn[2]
+
+			losses[iter_num, 2] = loss_class[1]
+			losses[iter_num, 3] = loss_class[2]
+			losses[iter_num, 4] = loss_class[3]
+
+			progbar.update(iter_num+1, [('rpn_cls', losses[iter_num, 0]), ('rpn_regr', losses[iter_num, 1]),
+									  ('detector_cls', losses[iter_num, 2]), ('detector_regr', losses[iter_num, 3])])
+
+			iter_num += 1
+
+			if iter_num == epoch_length:
+				loss_rpn_cls = np.mean(losses[:, 0])
+				loss_rpn_regr = np.mean(losses[:, 1])
+				loss_class_cls = np.mean(losses[:, 2])
+				loss_class_regr = np.mean(losses[:, 3])
+				class_acc = np.mean(losses[:, 4])
+
+				mean_overlapping_bboxes = float(sum(rpn_accuracy_for_epoch)) / len(rpn_accuracy_for_epoch)
+				rpn_accuracy_for_epoch = []
+
+				curr_loss = loss_rpn_cls + loss_rpn_regr + loss_class_cls + loss_class_regr
+				iter_num = 0
+				start_time = time.time()
+
+				if curr_loss < best_loss:
+					best_loss = curr_loss
+					model_all.save_weights(C.model_path)
+
+				break
+
+		except Exception as e:
+			print('Exception: {}'.format(e))
+			continue
