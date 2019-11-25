a3044e29a5ee983a3f931ee1e77b857d45aee4f1
jennifer2020 <jninotapia@gmail.com>
2019-11-24 20:53:43 -0500

    takes too long to train ):

--- test_file.py
+++ test_file.py
@@ -1,129 +1,102 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+
+
 import gzip
 import numpy as np
-
 import json
-token = {"username":"jennifer2020","key":"ecdd8b3ac0496be65f1c03e5c389a939"}
-with open('/content/.kaggle/kaggle.json', 'w') as file:
-    json.dump(token, file)
-!cp /content/.kaggle/kaggle.json ~/.kaggle/kaggle.json
-!kaggle config set -n path -v{/content}
-!chmod 600 /root/.kaggle/kaggle.json
-!kaggle competitions download -c rsna-pneumonia-detection-challenge -p /content
-!unzip \*.zip
-!ls
-
-!pip install pydicom
 import pydicom
+import glob
 import pandas as pd
-import tensorflow as tf
-import gzip
-import numpy as np
-d = pd.read_csv('/content/stage_2_train_labels.csv')
-d.head()
-dcm_file = 'fffec09e-8a4a-48b1-b33e-ab4890ccd136.dcm'
-dcm_data = pydicom.read_file(dcm_file)
-
-"""
-print(dcm_data)
-im = dcm_data.pixel_array
-print(type(im))
-print(im.dtype)
-print(im.shape)
 import matplotlib.pyplot as plt
-plt.imshow(im, cmap=plt.cm.bone)
-"""
+import csv
 
+train_img_p = 'rsna-pneumonia-detection-challenge/stage_2_train_images/'
+test_imgp = 'rsna-pneumonia-detection-challenge/stage_2_test_images/'
+train_csv = 'rsna-pneumonia-detection-challenge/stage_2_train_labels.csv'
 
-info = d.to_dict('index')
-pnemonia = {}
-
-for key in info:
-  pid = info[key]['patientId']
-  pnemonia[pid] = info[key]['Target']
-
-limit = 0
-
-small_train_image_arrs = []
+train_files = []
+train_labels = []
 train_images = []
-small_train_labels = []
 
-image_path_to_type = {}
-image_fp = []
-image_lb = []
-for pid in pnemonia:
-  if limit == 2000:
-    break
-  fn = pid + '.dcm'
-  image_path_to_type[fn] = pnemonia[pid]
-  image_fp.append(fn)
-  dcm_data = pydicom.read_file(fn)
+with open(train_csv, 'r', newline='') as f:
+    reader = csv.reader(f)
 
-  # train_images.append(dcm_data)
-  train_images.append(dcm_data.pixel_array.flatten())
-  # small_train_labels.append(pnemonia[pid])
-  image_lb.append(pnemonia[pid])
-  limit += 1
-image_lb = np.array(image_lb)
+    next(reader)
+    ct = 0
+    for row in reader:
+        if ct == 500:
+            break
+        fn = train_img_p + row[0] + '.dcm'
+        dcm_data = pydicom.read_file(fn)
+        label = int(row[5])
+        train_images.append(dcm_data.pixel_array.flatten())
+        train_files.append(fn)
+        train_labels.append(label)
+        ct += 1
+
+train_files = np.array(train_files)
+train_labels = np.array(train_labels)
 train_images = np.array(train_images)
-print(train_images.shape)
+
+
 train_images = np.transpose(\
-		np.reshape(train_images,(-1, 4, 512 ,512)),[0,2,3,1])
-#dd = pd.DataFrame(list(zip(image_fp,image_cls)),columns=['filename','class'])
-train_images.shape
-# Has 4 ouptut channels
+		np.reshape(train_images,(-1,1,1024,1024)),[0,2,3,1])
+print(train_images.shape)
 
+# Has 1 ouptut channel
 
-
-# print("pneumonia path: {}".format(files_path))
-# !ls data -l --block-size=M
-
-# !mkdir data/
-# !mkdir fp_data/
-"""
-zip_loc = '/content'
-train_dir = loc + '/train'
-test_dir = loc + '/test'
-"""
-
-BATCH_SIZE = 128
-EPOCHS = 10
+BATCH_SIZE = 32 # 64
+EPOCHS = 5 # 10
 IMG_HEIGHT = 1024
 IMG_WIDTH = 1024
-TRAIN_SIZE = 2000
-TEST_SIZE = 2000
+TRAIN_SIZE = 500
+TEST_SIZE = 500
 
 
 # Generators for images
 train_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1.0/255,
-    rotation_range=45,
+    rotation_range=60,
     horizontal_flip=True)
 
 test_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255,
-    rotation_range=45,
+    rotation_range=60,
     horizontal_flip=True)
-
-# t = np.array(small_train_image_arrs).reshape((128, 1024, 1024))
-# train_images = np.zeros((128, 1024, 1024))
-# Input data. Numpy array of rank 4 or a tuple.
-
-train_labels = np.zeros((1,100))
-
-train_image_generator.flow(
+# train_image_generator.fit(train_images)
+f=train_image_generator.flow(
     train_images,
-    image_lb,
+    train_labels,
     batch_size=32,
-    shuffle=True
-)
+    shuffle=True)
 
-model = tf.keras.Sequential([
-  Conv2D(16,kernel_size=(5,5),strides=(2,2), padding='same', activation="relu", use_bias=True, kernel_initializer='glorot_uniform'),
-  MaxPool2D(pool_size=(3, 3), strides=(2,2), padding='same'),
-  Dropout(0.3)
+"""
+# View images
+for train_b, labels_b in f:
+    for i in range(0,12):
+        print("IS: ", labels_b[i])
+        plt.imshow(train_b[i].reshape(1024, 1024))
+        plt.show()
+        plt.clf()
+    break
+exit(0)
+"""
+
+model = Sequential([
+    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,1)),
+    MaxPooling2D(),
+    Conv2D(20, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    # Conv2D(20, 3, padding='same', activation='relu'),
+    # MaxPooling2D(),
+    Flatten(),
+    # Dense(512, activation='relu'),
+    Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.01), loss="sparse_categorical_crossentropy")
-model.fit_generator(train_image_generator, steps_per_epoch=((train_size // BATCH_SIZE) + 1), epochs=EPOCHS, verbose=2)
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="binary_crossentropy",metrics=['accuracy'])
+model.fit_generator(f, steps_per_epoch=((TRAIN_SIZE // BATCH_SIZE) + 1), epochs=EPOCHS)
